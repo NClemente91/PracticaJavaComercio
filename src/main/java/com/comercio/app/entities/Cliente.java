1aad7f26814682a7f9fdf20572999cd9280f9f8d
Leonel Altamirano <altamiranopedroleonel@outlook.com>
2022-09-19 15:18:35 -0300

    Commit 3 - Entidades completadas

--- src/main/java/com/comercio/app/entities/Cliente.java
+++ src/main/java/com/comercio/app/entities/Cliente.java
@@ -1,4 +1,24 @@
 package com.comercio.app.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false, length = 20)
+    private String nombre;
+
+    @Column(name = "apellido", nullable = false, length = 20)
+    private String apellido;
+
+    @Column(name = "telefono", nullable = false, length = 20)
+    private String telefono;
+
+    @Column(name = "email", nullable = false, length = 40)
+    private String email;
 }
--- src/main/java/com/comercio/app/entities/Producto.java
+++ src/main/java/com/comercio/app/entities/Producto.java
@@ -1,4 +1,19 @@
 package com.comercio.app.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false, length = 25)
+    private String nombre;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
 }
