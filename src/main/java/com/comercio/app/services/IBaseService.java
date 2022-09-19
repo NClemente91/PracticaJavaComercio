127b8791b26520e2e0f2a836347f07a8ce477a14
NClemente91 <nicoclemente91@gmail.com>
2022-09-19 16:14:01 -0300

    Avance en repositorio y servicio de cliente

--- src/main/java/com/comercio/app/services/IBaseService.java
+++ src/main/java/com/comercio/app/services/IBaseService.java
@@ -1,4 +1,11 @@
 package com.comercio.app.services;
 
-public interface IBaseService {
+import java.util.List;
+
+public interface IBaseService<E> {
+    public List<E> findAll() throws Exception;
+    public E findById(Long id);
+    public E insert(E entity);
+    public E update(Long id, E entity);
+    public boolean delete(Long id);
 }
--- src/main/java/com/comercio/app/services/impl/ClienteService.java
+++ src/main/java/com/comercio/app/services/impl/ClienteService.java
@@ -1,4 +1,44 @@
 package com.comercio.app.services.impl;
 
-public class ClienteService {
+import com.comercio.app.entities.Cliente;
+import com.comercio.app.repositories.IBaseRepository;
+import com.comercio.app.services.IBaseService;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class ClienteService implements IBaseService<Cliente> {
+
+    private EntityManager em;
+    private IBaseRepository<Cliente> clienteRepository;
+
+    public ClienteService(EntityManager em, IBaseRepository<Cliente> clienteRepository) {
+        this.em = em;
+        this.clienteRepository = clienteRepository;
+    }
+
+    @Override
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Cliente insert(Cliente entity) {
+        return null;
+    }
+
+    @Override
+    public Cliente update(Long id, Cliente entity) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
 }
